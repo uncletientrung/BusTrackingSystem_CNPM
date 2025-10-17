7a4d1237525c9c4faa2d631e95bd6603561d5ffa
uncletientrung <thihachcf@gmail.com>
2025-10-18 02:32:56 +0700

    Them Component ProtectedRoute

---
+++ frontend/src/components/Auth/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { useLocation } from "react-router-dom"
+
+export default function ProtectedRoute({ children, allowedRoles = [] }) {
+    // // const { isAuthenticated, user } = useAuthStore()
+    // const location = useLocation()
+
+    // if (!isAuthenticated) {
+    //     return <Navigate to="/login" state={{ from: location }} replace />
+    // }
+
+    // if (allowedRoles.length > 0 && user && !allowedRoles.includes(user.role)) {
+    //     return <UnauthorizedPage />
+    // }
+    // return children
+};
